c4175f2191b069ee78f1d6fa9c3e2909abdb03c8
Akseli Lahtinen <akselmo@akselmo.dev>
2022-10-30 23:26:13 +0200

    simplify xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,17 +1,6 @@
 add_rules("mode.debug")
 
-package("raylib_git")
-    add_deps("cmake")
-    set_sourcedir("/home/akseli/Repositories/raylib/")
-    on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
-        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.cmake").install(package, configs)
-    end)
-package_end()
-
-add_requires("raylib_git")
+add_requires("raylib master", {system = false})
 set_defaultmode("debug")
 set_warnings("allextra", "conversion")
 
@@ -19,7 +8,7 @@ target("artificial-rage")
 
     set_kind("binary")
     add_files("src/*.c")
-    add_packages("raylib_git")
+    add_packages("raylib")
     set_languages("c99", "c++11")
 
     after_build(function (target)
