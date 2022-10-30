2562613235fd2e99a72797723f7c547fb868df08
Akseli Lahtinen <akselmo@akselmo.dev>
2022-10-30 22:42:47 +0200

    remove cmakefile, use raylib git version

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,17 @@
-add_rules("mode.debug", "mode.release")
-add_requires("raylib master", {system = false})
+add_rules("mode.debug")
+
+package("raylib_git")
+    add_deps("cmake")
+    set_sourcedir("/home/akseli/Repositories/raylib/")
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+        import("package.tools.cmake").install(package, configs)
+    end)
+package_end()
+
+add_requires("raylib_git")
 set_defaultmode("debug")
 set_warnings("allextra", "conversion")
 
@@ -7,7 +19,7 @@ target("artificial-rage")
 
     set_kind("binary")
     add_files("src/*.c")
-    add_packages("raylib")
+    add_packages("raylib_git")
     set_languages("c99", "c++11")
 
     after_build(function (target)
