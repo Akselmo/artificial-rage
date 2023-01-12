81ef32d83161c6c8186a9522e237298d9a266397
Akseli Lahtinen <akselmo@akselmo.dev>
2023-01-12 23:43:09 +0200

    update from c99 to c11

--- xmake.lua
+++ xmake.lua
@@ -11,7 +11,7 @@ target("artificial-rage")
     set_kind("binary")
     add_files("src/*.c")
     add_packages("raylib")
-    set_languages("c99", "c++11")
+    set_languages("c11", "c++11")
     --    add_cflags("-fanalyzer")
 
     after_build(function (target)
